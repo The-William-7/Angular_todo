41345e9f1c45e9391300db44fcc1f2b0339ef890
The-William-7 <hernandez99william@gmail.com>
2025-11-23 23:05:34 -0600

    Capetas

---
+++ Proyecto_Final/sistema_videojuegos/src/app/modelos/juego.model.spec.ts
@@ -0,0 +1,7 @@
+import { Juego } from './juegomodel';
+
+describe('Juego', () => {
+  it('should create an instance', () => {
+    expect(new Juego()).toBeTruthy();
+  });
+});
---
+++ Proyecto_Final/sistema_videojuegos/src/app/modelos/pedido.model.spec.ts
@@ -0,0 +1,7 @@
+import { Pedido } from './pedidomodel';
+
+describe('Pedido', () => {
+  it('should create an instance', () => {
+    expect(new Pedido()).toBeTruthy();
+  });
+});
